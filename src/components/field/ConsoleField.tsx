70807f24750e770b89126b0360de5bedf3de3eaf
Robert Merriman <robertbmerriman@gmail.com>
2026-06-24 22:04:47 +0100

    Console data and build up UI

--- src/components/field/ConsoleField.tsx
+++ src/components/field/ConsoleField.tsx
@@ -8,6 +8,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import { consoleTypes, consolesByType } from '@/lib/consoleData'
 import { useConsoleStore } from '@/stores/consoleStore'
 
 export function ConsoleField() {
@@ -23,34 +24,18 @@ export function ConsoleField() {
             <SelectTrigger>
               <SelectValue placeholder="Select a console" />
             </SelectTrigger>
+
             <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Handhelds</SelectLabel>
-                <SelectItem value="poke-mini">Pokémon mini (96x64)</SelectItem>
-                <SelectItem value="lynx">Lynx (160x102)</SelectItem>
-                <SelectItem value="gb">Game Boy/Game Gear (160x144)</SelectItem>
-                <SelectItem value="ngp">Neo Geo Pocket (160x152)</SelectItem>
-                <SelectItem value="ws">WonderSwan (224x144)</SelectItem>
-                <SelectItem value="gba">Game Boy Advance (240x160)</SelectItem>
-                <SelectItem value="nds-single">Nintendo DS □ (256x192)</SelectItem>
-                <SelectItem value="nds-horizontal">Nintendo DS ◫ (512x192)</SelectItem>
-                <SelectItem value="nds-vertical">Nintendo DS ⊟ (256x384)</SelectItem>
-                <SelectItem value="psp">PSP (480x272)</SelectItem>
-              </SelectGroup>
-              <SelectGroup>
-                <SelectLabel>Home consoles</SelectLabel>
-                <SelectItem value="nes">Nintendo (256x240)</SelectItem>
-                <SelectItem value="genesis">Genesis (320x224)</SelectItem>
-                <SelectItem value="snes">Super Nintendo (256x224)</SelectItem>
-                <SelectItem value="psx">PlayStation (320x240)</SelectItem>
-                <SelectItem value="n64">Nintendo 64 (320x240)</SelectItem>
-                <SelectItem value="dc">Dreamcast (640x480)</SelectItem>
-              </SelectGroup>
-              <SelectGroup>
-                <SelectLabel>Other</SelectLabel>
-                <SelectItem value="pico-8">PICO-8 (128x128)</SelectItem>
-                <SelectItem value="vb">Virtual Boy (384x224)</SelectItem>
-              </SelectGroup>
+              {consoleTypes.map((type) => (
+                <SelectGroup key={type}>
+                  <SelectLabel>{type}</SelectLabel>
+                  {consolesByType[type].map(({ tag, name, resX, resY }) => (
+                    <SelectItem value={tag} key={tag}>
+                      {name} ({resX}x{resY})
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              ))}
             </SelectContent>
           </Select>
         </FieldGroup>
---
+++ src/lib/consoleData.ts
@@ -0,0 +1,166 @@
+export const consoleTypes = ['Handheld', 'Home console', 'Other'] as const
+export type ConsoleType = (typeof consoleTypes)[number]
+
+export const consoleTags = [
+  'poke-mini',
+  'lynx',
+  'gb',
+  'ngp',
+  'ws',
+  'gba',
+  'nds-single',
+  'nds-horizontal',
+  'nds-vertical',
+  'psp',
+  'nes',
+  'genesis',
+  'snes',
+  'psx',
+  'n64',
+  'dc',
+  'pico-8',
+  'vb',
+] as const
+export type ConsoleTag = (typeof consoleTags)[number]
+
+export interface Console {
+  tag: ConsoleTag
+  name: string
+  resX: number
+  resY: number
+  type: ConsoleType
+}
+export const consoles: Console[] = [
+  {
+    tag: 'poke-mini',
+    name: 'Pokémon mini',
+    resX: 96,
+    resY: 64,
+    type: 'Handheld',
+  },
+  {
+    tag: 'lynx',
+    name: 'Lynx',
+    resX: 160,
+    resY: 102,
+    type: 'Handheld',
+  },
+  {
+    tag: 'gb',
+    name: 'Game Boy/Game Gear',
+    resX: 160,
+    resY: 144,
+    type: 'Handheld',
+  },
+  {
+    tag: 'ngp',
+    name: 'Neo Geo Pocket',
+    resX: 160,
+    resY: 152,
+    type: 'Handheld',
+  },
+  {
+    tag: 'ws',
+    name: 'WonderSwan',
+    resX: 224,
+    resY: 144,
+    type: 'Handheld',
+  },
+  {
+    tag: 'gba',
+    name: 'Game Boy Advance',
+    resX: 240,
+    resY: 160,
+    type: 'Handheld',
+  },
+  {
+    tag: 'nds-single',
+    name: 'Nintendo DS □',
+    resX: 256,
+    resY: 192,
+    type: 'Handheld',
+  },
+  {
+    tag: 'nds-horizontal',
+    name: 'Nintendo DS ◫',
+    resX: 512,
+    resY: 192,
+    type: 'Handheld',
+  },
+  {
+    tag: 'nds-vertical',
+    name: 'Nintendo DS ⊟',
+    resX: 256,
+    resY: 384,
+    type: 'Handheld',
+  },
+  {
+    tag: 'psp',
+    name: 'PSP',
+    resX: 480,
+    resY: 272,
+    type: 'Handheld',
+  },
+  {
+    tag: 'nes',
+    name: 'Nintendo',
+    resX: 256,
+    resY: 240,
+    type: 'Home console',
+  },
+  {
+    tag: 'genesis',
+    name: 'Genesis',
+    resX: 320,
+    resY: 224,
+    type: 'Home console',
+  },
+  {
+    tag: 'snes',
+    name: 'Super Nintendo',
+    resX: 256,
+    resY: 224,
+    type: 'Home console',
+  },
+  {
+    tag: 'psx',
+    name: 'PlayStation',
+    resX: 320,
+    resY: 240,
+    type: 'Home console',
+  },
+  {
+    tag: 'n64',
+    name: 'Nintendo 64',
+    resX: 320,
+    resY: 240,
+    type: 'Home console',
+  },
+  {
+    tag: 'dc',
+    name: 'Dreamcast',
+    resX: 640,
+    resY: 480,
+    type: 'Home console',
+  },
+  {
+    tag: 'pico-8',
+    name: 'PICO-8',
+    resX: 128,
+    resY: 128,
+    type: 'Other',
+  },
+  {
+    tag: 'vb',
+    name: 'Virtual Boy',
+    resX: 384,
+    resY: 224,
+    type: 'Other',
+  },
+]
+
+export type ConsolesByType = Record<ConsoleType, Omit<Console, 'type'>[]>
+export const consolesByType: ConsolesByType = { Handheld: [], 'Home console': [], Other: [] }
+consoles.forEach((console) => {
+  consolesByType[console.type].push(console)
+})
