502e32c9ccd553dda38cf1ffdd5ac2b211d38939
Robert Merriman <robertbmerriman@gmail.com>
2026-06-29 15:07:36 +0100

    ScreenSelect component

--- src/components/field/ScreenField.tsx
+++ src/components/field/ScreenField.tsx
@@ -1,26 +1,16 @@
+import { ScreenSelect } from '@/components/screen/ScreenSelect'
 import { Field, FieldGroup, FieldLabel, FieldSet } from '@/components/ui/field'
 import {
   Select,
   SelectContent,
-  SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
-import {
-  screenGroupingOptions,
-  screenManufacturers,
-  screenRatioTypes,
-  screensByManufacturer,
-  screensByRatio,
-  screensByRes,
-} from '@/lib/screenData'
+import { screenGroupingOptions } from '@/lib/screenData'
 import { useScreenStore } from '@/stores/screenStore'
 
 export function ScreenField() {
-  const tag = useScreenStore((state) => state.tag)
-  const setScreen = useScreenStore((state) => state.setScreenByTag)
   const grouping = useScreenStore((state) => state.grouping)
   const setGrouping = useScreenStore((state) => state.setGrouping)
 
@@ -30,57 +20,9 @@ export function ScreenField() {
         <FieldGroup className="flex flex-row gap-4">
           <Field>
             <FieldLabel>Device</FieldLabel>
-            <Select defaultValue={tag} onValueChange={setScreen}>
-              <SelectTrigger>
-                <SelectValue placeholder="Select a screen" />
-              </SelectTrigger>
-
-              <SelectContent>
-                {grouping === 'Aspect ratio' &&
-                  screenRatioTypes.map((ratio) => (
-                    <SelectGroup key={ratio}>
-                      <SelectLabel>{ratio}</SelectLabel>
-                      {screensByRatio[ratio].map(
-                        ({ tag, manufacturer, name, resX, resY, sizeInches, ratioX, ratioY }) => (
-                          <SelectItem value={tag} key={tag}>
-                            {ratio === 'Other' && `[${ratioX}:${ratioY}]`} {manufacturer} {name}
-                            {/* [{resX}x{resY} - {sizeInches}"] */}
-                          </SelectItem>
-                        ),
-                      )}
-                    </SelectGroup>
-                  ))}
-                {grouping === 'Manufacturer' &&
-                  screenManufacturers.map((manufacturer) => (
-                    <SelectGroup key={manufacturer}>
-                      <SelectLabel>{manufacturer}</SelectLabel>
-                      {screensByManufacturer[manufacturer].map(
-                        ({ tag, manufacturer, name, resX, resY, sizeInches, ratioX, ratioY }) => (
-                          <SelectItem value={tag} key={tag}>
-                            {manufacturer} {name}
-                            {/* [{ratioX}:{ratioY} - {resX}x{resY} - {sizeInches}"] */}
-                          </SelectItem>
-                        ),
-                      )}
-                    </SelectGroup>
-                  ))}
-                {grouping === 'Resolution' &&
-                  Object.keys(screensByRes).map((res) => (
-                    <SelectGroup key={res}>
-                      <SelectLabel>
-                        {res} - {screensByRes[res][0]?.ratioX}:{screensByRes[res][0]?.ratioY}
-                      </SelectLabel>
-                      {screensByRes[res].map(({ tag, manufacturer, name, sizeInches }) => (
-                        <SelectItem value={tag} key={tag}>
-                          {manufacturer} {name}
-                          {/* [{sizeInches}"] */}
-                        </SelectItem>
-                      ))}
-                    </SelectGroup>
-                  ))}
-              </SelectContent>
-            </Select>
+            <ScreenSelect />
           </Field>
+
           <Field className="w-fit">
             <FieldLabel>Group by</FieldLabel>
             <Select defaultValue={grouping} onValueChange={setGrouping}>
