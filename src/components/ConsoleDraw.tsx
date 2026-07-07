4ff53d12c3f6a8024089907d1ea133b765189eaa
Robert Merriman <robertbmerriman@gmail.com>
2026-07-07 18:06:42 +0100

    Insufficent screen resolution = not worth rendering everything

--- src/components/ConsoleDraw.tsx
+++ src/components/ConsoleDraw.tsx
@@ -4,6 +4,7 @@ import type { Screen } from '@/lib/screenData'
 import { cn, findDiagonal } from '@/lib/utils'
 import { useConsoleStore } from '@/stores/consoleStore'
 import type { CustomScreen } from '@/stores/customScreenStore'
+// import { useSettingsStore } from '@/stores/settingsStore'
 
 interface Props {
   screen: Screen | CustomScreen
@@ -11,6 +12,8 @@ interface Props {
   consol: Console
 }
 export default function ConsoleDraw({ screen, screenDiagonalPixels, consol }: Props) {
+  // const screensMaxDisplayWidth = useSettingsStore((s) => s.screensMaxDisplayWidth)
+
   const integerScaling = useConsoleStore((state) => state.integerScaling)
   const cropOverscan = useConsoleStore((state) => state.cropOverscan)
 
@@ -29,14 +32,15 @@ export default function ConsoleDraw({ screen, screenDiagonalPixels, consol }: Pr
   const consoleScaledResX = consol.resX * consoleScale
   const consoleScaledResY = consol.resY * consoleScale
 
-  // const consoleDiagonalPixels = findDiagonal(consoleScaledResX, consoleScaledResY)
   const consoleXInches = screen.sizeInches * (consoleScaledResX / screenDiagonalPixels)
   const consoleYInches = screen.sizeInches * (consoleScaledResY / screenDiagonalPixels)
   const consoleSizeInches = findDiagonal(consoleXInches, consoleYInches)
-  // const consolePpi = Math.round(consoleDiagonalPixels / consoleSizeInches)
+  // const consoleDiagonalPixels = findDiagonal(consoleScaledResX, consoleScaledResY)
 
   const croppedX = Math.max((consoleScaledResX - screen.resX) / (consoleScale * 2), 0)
   const croppedY = Math.max((consoleScaledResY - screen.resY) / (consoleScale * 2), 0)
+
+  const notWorthIt = consoleScaleRaw < 1
 
   return (
     <div className="flex flex-col gap-1">
@@ -74,17 +78,20 @@ export default function ConsoleDraw({ screen, screenDiagonalPixels, consol }: Pr
       >
         {/* Overscan */}
         <div
-          className="bg-destructive/40 absolute -z-50"
+          className={cn('bg-destructive/40 absolute -z-50', { hidden: !notWorthIt })}
           style={{
             width: consoleXInches + 'in',
             height: consoleYInches + 'in',
             left: `calc(50% - ${consoleXInches / 2}in)`,
             top: `calc(50% - ${consoleYInches / 2}in)`,
           }}
-        ></div>
+        />
         {/* Console */}
         <div
-          className="z-0 flex flex-col items-center justify-center bg-gray-400 text-center text-gray-900"
+          className={cn(
+            'z-0 flex flex-col items-center justify-center bg-gray-400 text-center text-gray-900',
+            { 'bg-destructive/60': notWorthIt },
+          )}
           style={{
             width: consoleXInches + 'in',
             height: consoleYInches + 'in',
@@ -103,13 +110,12 @@ export default function ConsoleDraw({ screen, screenDiagonalPixels, consol }: Pr
               {consoleScale.toLocaleString(undefined, { maximumFractionDigits: 1 })}x
             </span>
           </span>
-          {(croppedX > 0 || croppedY > 0) && ( // There is an issue with these elements overlapping with transparency, one solution would be to change to a solid color or another would be to clamp the inner console size to the screen bounds and draw behind it
-            <>
-              <span>
-                (cropped: {Math.ceil(croppedX)},{Math.ceil(croppedY)})
-              </span>
-            </>
+          {(croppedX > 0 || croppedY > 0) && (
+            <span>
+              (cropped: {Math.ceil(croppedX)},{Math.ceil(croppedY)})
+            </span>
           )}
+          {notWorthIt && <span>Insufficient screen resolution to render at x1</span>}
         </div>
       </div>
     </div>
